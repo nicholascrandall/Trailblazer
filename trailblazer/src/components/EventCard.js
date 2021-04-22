e4bb6fd5bc9293548e879440cd173d2297a4acc4
Ellyn Golden <ejgolden94@gmail.com>
2021-04-21 20:06:48 -0400

    converting event card to class component

--- trailblazer/src/components/EventCard.js
+++ trailblazer/src/components/EventCard.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import {Card, Icon} from 'semantic-ui-react'
+import {Card, Icon, Image} from 'semantic-ui-react'
 import {Link} from 'react-router-dom'
 
-export default function EventCard (props){
+export default class EventCard extends{
     const spotsLeft = props.event.maxAttendees - props.event.attendees.length
     const attendees = (
         <>
@@ -11,17 +11,24 @@ export default function EventCard (props){
           <Link to='/event/edit'><Icon name="edit" onClick={()=>{props.setEvent(props.event)}}/></Link>
         </>
       )
+    const image = (
+      <>
+      <Link to='/event/meetup'>
+        <Image src={props.event.img} wrapped ui={false} />
+      </Link>
+      </>
+    )
     const d = new Date(props.event.date)
     return (
-        <Link to='/event/meetup'>
+        // <Link to='/event/meetup'>
           <Card
-              image={props.event.img}
+              image={image}
               header={props.event.name}
               meta={d.toDateString()}
               description={props.event.details.activityType + ' level ' + props.event.details.difficulty}
               extra={attendees}
               onClick={() => props.setEvent(props.event)}
           />
-        </Link>
+        // </Link>
     )
 }
